e0caed5ace14dfe07c65613ce2a08b0e18396e42
Slava <slava.bandura1@gmail.com>
2020-03-17 23:17:51 +0200

    fix bug

--- part2/index.js
+++ part2/index.js
@@ -1,4 +1,3 @@
-
 const toggleOpenMenus = document.querySelectorAll(".js-open-menu");
 const menus = document.querySelectorAll(".js-menu");
 
@@ -7,6 +6,10 @@ toggleOpenMenus.forEach((btn, idx) => {
 });
 
 function handlOpenMenu(idx) {
+	let one = document.querySelectorAll('.menu-open');
+	let two = document.querySelectorAll('.b-r-open');
+	if (!one.length == 0) {one[0].classList.remove('menu-open');};
+	if (!two.length == 0) {two[0].classList.remove('b-r-open');};
   menus[idx].classList.toggle("menu-open");
   toggleOpenMenus[idx].classList.toggle("b-r-open");
 }
